9805f7b22053becb11671987ddcf04b0b5e75bd7
Vinicius Davi <viniciuschwalensky@gmail.com>
2021-05-18 02:00:43 -0300

    Atualização nos scripts

---
+++ website/scripts/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".showPassword, .hidePassword").on('click', function () {
+        var passwordId = $(this).parent('.inputWrapper:first').find('input').attr('id');
+        if ($(this).hasClass('showPassword')) {
+            $("#" + passwordId).attr("type", "text");
+            $(this).parent().find(".showPassword").hide();
+            $(this).parent().find(".hidePassword").show();
+        } else {
+            $("#" + passwordId).attr("type", "password");
+            $(this).parent().find(".hidePassword").hide();
+            $(this).parent().find(".showPassword").show();
+        }
+    });
+});
+
+/*
+var modalCenter = document.getElementById('modalCenter')
+var triggerModal = document.getElementById('triggerModal')
+
+modalCenter.addEventListener('shown.bs.modal', function () {
+  triggerModal.focus()
+})
+*/
+
+/* Smooth Scroll */
