1fa2bdd865671ebcbacd67627a0ac5b481e1fdc0
Vinicius Davi <viniciuschwalensky@gmail.com>
2021-05-28 21:42:40 -0300

    Atualização geral no Fale Conosco, Acessar e Cadastrar

--- website/scripts/script.js
+++ website/scripts/script.js
@@ -1,27 +1,3 @@
-$(document).ready(function () {
-    $(".showPassword, .hidePassword").on('click', function () {
-        var passwordId = $(this).parent('.inputWrapper:first').find('input').attr('id');
-        if ($(this).hasClass('showPassword')) {
-            $("#" + passwordId).attr("type", "text");
-            $(this).parent().find(".showPassword").hide();
-            $(this).parent().find(".hidePassword").show();
-        } else {
-            $("#" + passwordId).attr("type", "password");
-            $(this).parent().find(".hidePassword").hide();
-            $(this).parent().find(".showPassword").show();
-        }
-    });
-});
-
-/*
-var modalCenter = document.getElementById('modalCenter')
-var triggerModal = document.getElementById('triggerModal')
-
-modalCenter.addEventListener('shown.bs.modal', function () {
-  triggerModal.focus()
-})
-*/
-
 // Visão geral
 
 // Organização
@@ -94,4 +70,19 @@ $('#btn--desempenho').on('click', function () {
 
     // Image
     $('#visao-geral--img').attr('src', '../assets/illustrations/desempenho.svg');
+});
+
+// Acessar
+$('#permanecer-conectado').on('click', function () {
+    let checkbox_value = $('#checkbox').prop('checked');
+
+    if (checkbox_value === false) {
+        $('#checkbox').prop('checked', true);
+        $('.hasborder').toggleClass('filled');
+    }
+
+    if (checkbox_value) {
+        $('#checkbox').prop('checked', false);
+        $('.hasborder').toggleClass('filled');
+    }
 });
